45ffd269107df157b7f17b55825f4677cdbb9c26
krzychu124 <krzychu124@gmail.com>
2019-06-21 20:17:46 +0200

    Refactoring for other mods

--- RedirectionHelper.cs
+++ RedirectionHelper.cs
@@ -1,6 +1,6 @@
 ﻿/*
 The MIT License (MIT)
-Copyright (c) 2015 Sebastian Sch�ner
+Copyright (c) 2015 Sebastian Schöner
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -21,38 +21,65 @@ THE SOFTWARE.
 using System;
 using System.Reflection;
 
-namespace RedirectionFramework
-{
-
-    public class RedirectCallsState
-    {
-        public byte a, b, c, d, e;
-        public ulong f;
+namespace OtherCSMods.RedirectionFramework {
+    public struct RedirectCallsState {
+        public byte A, B, C, D, E;
+        public ulong F;
     }
 
     /// <summary>
     /// Helper class to deal with detours. This version is for Unity 5 x64 on Windows.
     /// We provide three different methods of detouring.
     /// </summary>
-    public static class RedirectionHelper
-    {
+    public static class RedirectionHelper {
         /// <summary>
         /// Redirects all calls from method 'from' to method 'to'.
         /// </summary>
         /// <param name="from"></param>
         /// <param name="to"></param>
-        public static RedirectCallsState RedirectCalls(MethodInfo from, MethodInfo to)
-        {
+        public static RedirectCallsState RedirectCalls(MethodInfo from, MethodInfo to) {
             // GetFunctionPointer enforces compilation of the method.
             var fptr1 = from.MethodHandle.GetFunctionPointer();
             var fptr2 = to.MethodHandle.GetFunctionPointer();
-            return PatchJumpTo(fptr1, fptr2);
+
+            RedirectCallsState ret = PatchJumpTo(fptr1, fptr2);
+
+            return ret;
         }
 
-        public static void RevertRedirect(MethodInfo from, RedirectCallsState state)
-        {
+        public static void RevertRedirect(MethodInfo from, RedirectCallsState state) {
             var fptr1 = from.MethodHandle.GetFunctionPointer();
             RevertJumpTo(fptr1, state);
+        }
+
+        public static bool IsRedirected(MethodInfo from, MethodInfo to) {
+            var fptr1 = from.MethodHandle.GetFunctionPointer();
+            var fptr2 = to.MethodHandle.GetFunctionPointer();
+            return IsRedirected(fptr1, fptr2);
+        }
+
+        private static byte[] GetRedir(IntPtr ptr) {
+            byte[] ret = new byte[13];
+            unsafe {
+                byte* pointer = (byte*) ptr.ToPointer();
+                for (int i = 0; i < 13; ++i) {
+                    ret[i] = *(pointer + i);
+                }
+            }
+
+            return ret;
+        }
+
+        public static bool IsRedirected(IntPtr site, IntPtr target) {
+            unsafe {
+                byte* sitePtr = (byte*) site.ToPointer();
+                return *sitePtr == 0x49 &&
+                       *(sitePtr + 1) == 0xBB &&
+                       *((ulong*) (sitePtr + 2)) == (ulong) target.ToInt64() &&
+                       *(sitePtr + 10) == 0x41 &&
+                       *(sitePtr + 11) == 0xFF &&
+                       *(sitePtr + 12) == 0xE3;
+            }
         }
 
         /// <summary>
@@ -61,55 +88,51 @@ namespace RedirectionFramework
         /// </summary>
         /// <param name="site"></param>
         /// <param name="target"></param>
-        public static RedirectCallsState PatchJumpTo(IntPtr site, IntPtr target)
-        {
-            RedirectCallsState state;
+        public static RedirectCallsState PatchJumpTo(IntPtr site, IntPtr target) {
+            RedirectCallsState state = GetState(site);
+
             // R11 is volatile.
-            unsafe
-            {
-                byte* sitePtr = (byte*)site.ToPointer();
-                state = GetState(sitePtr);
+            unsafe {
+                byte* sitePtr = (byte*) site.ToPointer();
 
                 *sitePtr = 0x49; // mov r11, target
                 *(sitePtr + 1) = 0xBB;
-                *((ulong*)(sitePtr + 2)) = (ulong)target.ToInt64();
+                *((ulong*) (sitePtr + 2)) = (ulong) target.ToInt64();
                 *(sitePtr + 10) = 0x41; // jmp r11
                 *(sitePtr + 11) = 0xFF;
                 *(sitePtr + 12) = 0xE3;
             }
+
             return state;
         }
 
-        private static unsafe RedirectCallsState GetState(byte* sitePtr)
-        {
-            var state = new RedirectCallsState
-            {
-                a = *sitePtr,
-                b = *(sitePtr + 1),
-                c = *(sitePtr + 10),
-                d = *(sitePtr + 11),
-                e = *(sitePtr + 12),
-                f = *((ulong*) (sitePtr + 2))
-            };
-            return state;
-        }
+        public static RedirectCallsState GetState(IntPtr site) {
+            RedirectCallsState state = new RedirectCallsState();
 
-        public static RedirectCallsState RevertJumpTo(IntPtr site, RedirectCallsState state)
-        {
-            RedirectCallsState detourState;
-            unsafe
-            {
-                byte* sitePtr = (byte*)site.ToPointer();
-                detourState = GetState(sitePtr);
-                *sitePtr = state.a; // mov r11, target
-                *(sitePtr + 1) = state.b;
-                *((ulong*)(sitePtr + 2)) = state.f;
-                *(sitePtr + 10) = state.c; // jmp r11
-                *(sitePtr + 11) = state.d;
-                *(sitePtr + 12) = state.e;
+            // R11 is volatile.
+            unsafe {
+                byte* sitePtr = (byte*) site.ToPointer();
+                state.A = *sitePtr;
+                state.B = *(sitePtr + 1);
+                state.C = *(sitePtr + 10);
+                state.D = *(sitePtr + 11);
+                state.E = *(sitePtr + 12);
+                state.F = *((ulong*) (sitePtr + 2));
             }
-            return detourState;
+
+            return state;
         }
 
+        public static void RevertJumpTo(IntPtr site, RedirectCallsState state) {
+            unsafe {
+                byte* sitePtr = (byte*) site.ToPointer();
+                *sitePtr = state.A; // mov r11, target
+                *(sitePtr + 1) = state.B;
+                *((ulong*) (sitePtr + 2)) = state.F;
+                *(sitePtr + 10) = state.C; // jmp r11
+                *(sitePtr + 11) = state.D;
+                *(sitePtr + 12) = state.E;
+            }
+        }
     }
 }
--- RedirectionUtil.cs
+++ RedirectionUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using OtherCSMods.RedirectionFramework;
 using RedirectionFramework.Attributes;
 
 namespace RedirectionFramework
